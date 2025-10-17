a43a5d240a632352f2250f1586e5fcf99330635a
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2025-10-17 13:43:16 -0400

    Updated versions

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/kmdparse/0.1.1")]
+#![doc(html_root_url = "https://docs.rs/kmdparse/0.0.1")]
 #![warn(missing_docs)]
 #![no_std]
 #![cfg_attr(docsrs, feature(doc_cfg))]
